0a65d37c5c40516e08bb09c1bf4dd25d6b94d78c
KrishnaHarishK <krishnaharish.k@gmail.com>
2015-08-01 03:23:50 -0700

    First Draft

---
+++ HelloHackathon.cpp
@@ -0,0 +1,18 @@
+/*
+ * HelloHackathon.cpp
+ *
+ *  Created on: Aug 1, 2015
+ *      Author: Administrator
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int
+main (int argc, char* argv[])
+{
+	std::cout << "Hello Hackathon!!!" << endl;
+
+	return 0;
+}
